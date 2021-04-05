7b2d850e16ccf2099881a076ab3c69f798f178db
pettermkuo <petterm0023@gmail.com>
2021-04-05 11:51:32 -0300

    first commit

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const usuariosController = require('../controllers/usuariosController');
+
+/* GET users listing. */
+router.get('/', usuariosController.index);
+
+
+
+module.exports = router;
